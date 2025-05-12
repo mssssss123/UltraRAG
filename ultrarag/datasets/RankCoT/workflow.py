85c65000755bd05691a9b8d0e0251db3e566b5e8
mssssss123 <824186479@qq.com>
2025-05-12 11:26:31 +0800

    first commit

---
+++ ultrarag/datasets/RankCoT/workflow.py
@@ -0,0 +1,12 @@
+import torch
+import torch.multiprocessing as mp
+from pathlib import Path
+home_path = Path().resolve()
+sys.path.append(home_path.as_posix())
+
+
+
+
+
+if __name__ == "__main__":
+    mp.set_start_method('spawn', force=True)
