9fd44e02240aa195540608a2651453f8dcec5e56
Haidong Xin <hdxin2002@gmail.com>
2025-08-26 16:20:44 +0800

    cleanup

--- scripts/run_generate_data.sh
+++
@@ -1,6 +0,0 @@
-python -m ultrarag.datasets.embedding.data_synth \
-    -api_key "token-abc123" \
-    -base_url "http://localhost:33389/v1" \
-    -model "Qwen2.5-7B-Instruct" \
-    -embed resource/models/minicpm-embedding-light \
-    -corpus resource/research/evorag/temp.jsonl
--- ultrarag/modules/kbalign/__init__.py
+++
@@ -1,1 +0,0 @@
-from .kbalign import KBAlign
