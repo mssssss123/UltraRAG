f55784af10de3d01424680b33b0b1d67c8b8a9ac
Haidong Xin <hdxin2002@gmail.com>
2026-01-20 16:55:32 +0800

    code review: servers/sayhello/src/sayhello.py (#248)

--- servers/sayhello/src/sayhello.py
+++ servers/sayhello/src/sayhello.py
@@ -7,6 +7,14 @@ app = UltraRAG_MCP_Server("sayhello")
 
 @app.tool(output="name->msg")
 def greet(name: str) -> Dict[str, str]:
+    """Greet a person by name.
+    
+    Args:
+        name: Name of the person to greet
+    
+    Returns:
+        Dictionary with 'msg' containing the greeting message
+    """
     ret = f"Hello, {name}!"
     app.logger.info(ret)
     return {"msg": ret}
